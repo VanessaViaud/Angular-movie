88bb99f4aabb88a8a5e50f86d29f9aa8fafa7c83
VanessaViaud <163421061+VanessaViaud@users.noreply.github.com>
2025-07-24 10:31:41 +0200

    feat/add-home-page

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import {MovieListComponent} from './pages/movie-list/movie-list.component';
+
+export const routes: Routes = [
+  {path: 'movies', component: MovieListComponent},
+];
---
+++ src/app/pages/star-rating/star-rating.component.ts
@@ -0,0 +1,12 @@
+import {Component, input, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-star-rating',
+  imports: [],
+  templateUrl: './star-rating.component.html',
+  styleUrl: './star-rating.component.scss'
+})
+export class StarRatingComponent {
+  rating = input(0);
+
+}
